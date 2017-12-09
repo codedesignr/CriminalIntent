7f6dddf78afd6c2f8a37590f39489f96b8c7ddb0
Adam Lopez <codedesignr@users.noreply.github.com>
2017-12-08 21:57:38 -0500

    Update CrimeList UI, add CrimeSolved Conditional image

--- app/src/main/java/com/rowfive/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/rowfive/criminalintent/CrimeFragment.java
@@ -55,7 +55,7 @@ public class CrimeFragment extends Fragment{
         mDateButton.setText(mCrime.getDate().toString());
         mDateButton.setEnabled(false);
 
-        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved_image);
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 mCrime.setSolved(isChecked);
